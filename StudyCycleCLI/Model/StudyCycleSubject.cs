18b934716f466b0b344e2c23e7db82ffd2380292
Antonio Mauricio <antonioimportant@gmail.com>
2025-05-07 09:02:11 -0300

    feat: adds StudyCycleSubject entity/database's table representation

---
+++ StudyCycleCLI/Model/StudyCycleSubject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyCycleCLI.Model
+{
+    internal class StudyCycleSubject
+    {
+        public int Id { get; set; }
+        public int StudyCycleId { get; set; }
+        public string Title { get; set; }
+        public Difficulty Difficulty { get; set; }
+        public AmountOfContent AmountOfContent { get; set; }
+        public double Weight { get; set; }
+        public int StudiedHours { get; set; }
+        public int MaxStudyHours { get; set; }
+        public int CompletedTimes { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? LastStudiedAt { get; set; }
+    }
+}
