45132e2f26add38eebf1aeba72d802727b5c700d
Antonio Mauricio <antonioimportant@gmail.com>
2025-05-14 19:04:51 -0300

    adds CYCLE UNSTUDY command

--- StudyCycleCLI/Program.cs
+++ StudyCycleCLI/Program.cs
@@ -32,7 +32,7 @@ namespace StudyCycleCLI
                             "edit", 
                             "addSubject", 
                             "study", 
-                            "unstduy", 
+                            "unstudy", 
                             "export", 
                             "delete"
                         ]
@@ -55,6 +55,8 @@ namespace StudyCycleCLI
                 if (entity == "cycle" && command == "complete") await TryToCompleteCycle(arguments);
                 if (entity == "cycle" && command == "view") ViewStudyCycle(arguments);
                 if (entity == "cycle" && command == "study") await StudyCycleSubject(arguments);
+                if (entity == "cycle" && command == "study") await StudyCycleSubject(arguments);
+                if (entity == "cycle" && command == "unstudy") await UnstudyCycleSubject(arguments);
             }
             catch (Exception e)
             {
@@ -64,6 +66,36 @@ namespace StudyCycleCLI
         }
 
         // Commands
+        static async Task UnstudyCycleSubject(string[] arguments)
+        {
+            if (arguments.Length == 0) throw new Exception("missing required argument: 'study cycle id'.");
+            if (arguments.Length < 3) throw new Exception("missing required argument: 'subject id'.");
+
+            var successfullyParsedStudyCycleId = int.TryParse(arguments[0], out var studyCycleId);
+            if (!successfullyParsedStudyCycleId) throw new Exception($"'{arguments[0]}' is an invalid id.");
+
+            var successfullyParsedSubjectId = int.TryParse(arguments[2], out var subjectId);
+            if (!successfullyParsedSubjectId) throw new Exception($"'{arguments[2]}' is an invalid id.");
+
+            using var db = new AppDbContext();
+
+            var studyCycle = db.StudyCycles.Include(sc => sc.Subjects).Where(sc => sc.Id == studyCycleId).FirstOrDefault();
+            if (studyCycle == null) throw new Exception($"study cycle with id '{studyCycleId}' was not found.");
+
+            var subject = studyCycle.Subjects.Where(sb => sb.Id == subjectId).FirstOrDefault();
+            if (subject == null) throw new Exception($"subject with id '{subjectId}' was not found.");
+
+            if (subject.StudiedHours == 0) throw new Exception($"'{subject.Title}' cannot be unstudied (studied hours is 0).");
+
+            subject.StudiedHours -= 1;
+
+            db.Update(studyCycle);
+
+            await db.SaveChangesAsync();
+
+            Console.WriteLine($"Removed 1h from '{subject.Title}'!");
+        }
+
         static async Task StudyCycleSubject(string[] arguments)
         {
             if (arguments.Length == 0) throw new Exception("missing required argument: 'study cycle id'.");
